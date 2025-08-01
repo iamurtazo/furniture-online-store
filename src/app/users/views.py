100e4e98509410672046700a56d134ac56912ee6
iamurtazo <murtazo207@inha.edu>
2025-08-01 12:11:03 +0900

    feat: implement user authentication system
    Configure template directories, add user auth views, templates, and URL routing.

--- src/app/users/views.py
+++ src/app/users/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login(request):
+    
+    context = {
+        'title': 'Home - Authorization'
+    }
+    
+    return render(request, 'users/login.html', context)
+
+def logout(request):
+    
+    context = {
+        'title': 'Home - Logout'
+    }
+    
+    return render(request, 'users/logout.html', context)
+
+def registration(request):
+    
+    context = {
+        'title': 'Home - Registration'
+    }
+    
+    return render(request, 'users/registration.html', context)
+
+def profile(request):
+    
+    context = {
+        'title': 'Home - Profile'
+    }
+    
+    return render(request, 'users/profile.html', context)
