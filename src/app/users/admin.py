7d8e9c07f4699bfa077ede0dbbdd62a0852b953a
iamurtazo <murtazo207@inha.edu>
2025-08-13 09:19:24 +0900

    Add functionalities to admin panel

--- src/app/users/admin.py
+++ src/app/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from carts.admin import CartTabAdmin
+from orders.admin import OrderTabularAdmin
 from .models import Users
 
 @admin.register(Users)
@@ -8,5 +9,5 @@ class UsersAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'email' ]
     search_fields = ['first_name', 'last_name', 'email']
     
-    inlines = [CartTabAdmin]
+    inlines = [CartTabAdmin, OrderTabularAdmin]
    
